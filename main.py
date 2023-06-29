a7124b5b255e0c8b104121f8c13919cdcadf0355
Shravan Asati <dev.shravan@protonmail.com>
2023-06-29 19:16:55 +0530

    show text info

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class Color(Enum):
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     RED = (188, 39, 50)
+    YELLOW = (255, 255, 0)
+    BLUE = (100, 149, 237)
 
 
 pygame.init()
@@ -19,6 +21,7 @@ BALL_RADIUS = 20
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Projectile Motion Simulator")
+FONT = pygame.font.SysFont("comicsans", 16)
 
 
 class Simulator:
@@ -26,13 +29,30 @@ class Simulator:
         self,
         initial_velocity: float,
         angle_of_projection: float,
-        horizontal_acceleration: float = 0,
+        horizontal_acceleration: float,
+        show_text: bool,
     ) -> None:
         self.engine = GroundToGround(
             initial_velocity, angle_of_projection, horizontal_acceleration
         )
+        self.show_text = show_text
+
         self.pos = 0
         self.trajectory = self.engine.trajectory(1 / FRAMERATE)
+
+    def show_info(self, window):
+        time_text = FONT.render(
+            f"Time of Flight: {round(self.engine.time_of_flight, 2)}s", 1, Color.BLUE.value
+        )
+        range_text = FONT.render(f"Range: {round(self.engine.range, 2)}m", 1, Color.BLUE.value)
+        hmax_text = FONT.render(
+            f"Maximum Height: {round(self.engine.hmax, 2)}m", 1, Color.BLUE.value
+        )
+
+        texts = [time_text, range_text, hmax_text]
+        centre = (WIDTH / 2, HEIGHT / 2)
+        for i, text in enumerate(texts):
+            window.blit(text, (centre[0], centre[1] + i * 15))
 
     @staticmethod
     def scale_coordinate(coordinate: tuple[int, int]):
@@ -40,11 +60,13 @@ class Simulator:
 
     def draw(self, window) -> bool:
         try:
-            coordinate = self.trajectory[self.pos]
+            coordinate = self.scale_coordinate(self.trajectory[self.pos])
             self.pos += 1
-            pygame.draw.circle(
-                window, Color.RED.value, self.scale_coordinate(coordinate), BALL_RADIUS
-            )
+
+            # draw the ball
+            pygame.draw.circle(window, Color.RED.value, (coordinate), BALL_RADIUS)
+
+            # draw tracing lines
             if len(self.trajectory[: self.pos]) > 2:
                 pygame.draw.lines(
                     window,
@@ -53,6 +75,24 @@ class Simulator:
                     tuple(map(self.scale_coordinate, self.trajectory[: self.pos])),
                     2,
                 )
+
+            if not self.show_text:
+                return False
+
+            self.show_info(window)
+
+            # draw distance and height
+            height_text = FONT.render(
+                f"Height: {((HEIGHT - coordinate[1]) / 1000):.2f}km",
+                True,
+                Color.YELLOW.value,
+            )
+            distance_text = FONT.render(
+                f"Distance: {(coordinate[0]/1000):.2f}km", True, Color.YELLOW.value
+            )
+            window.blit(height_text, (coordinate[0] - 10, coordinate[1] - 35))
+            window.blit(distance_text, (coordinate[0] - 10, coordinate[1] - 20))
+
         except IndexError:
             return True
 
@@ -60,7 +100,7 @@ class Simulator:
 def main():
     running = True
     clock = pygame.time.Clock()
-    sim = Simulator(300, 3, 15)
+    sim = Simulator(300, 30, 0, show_text=True)
     global SCALEX, SCALEY
     SCALEX = WIDTH / sim.engine.range
     SCALEY = HEIGHT / sim.engine.hmax
